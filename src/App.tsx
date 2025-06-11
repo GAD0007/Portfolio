ccf8b14fe91d22f85f861f5eac0334251b2bfaf2
GAD <adejumoibk702@gmail.com>
2025-06-11 23:15:19 +0100

    update: add new images and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -14,105 +14,71 @@ const sections = [
   { name: "Home", id: "home" },
   { name: "Skills", id: "skills" },
   { name: "Live Projects", id: "projects" },
-  { name: "Services", id: "services" },
-  { name: "Newsletter", id: "newsletter" },
   { name: "What Others Say", id: "what-others-say" },
-  { name: "Investments", id: "investments" },
-  { name: "Social", id: "social" },
+  { name: "Education", id: "education" },
+  { name: "Contact", id: "contact" },
 ];
 
 const works = [
   { id: 1, Poster: "React.png", Title: "React JS", Year: "2022" },
   { id: 2, Poster: "next.png", Title: "NEXT JS", Year: "2022" },
   { id: 3, Poster: "js.png", Title: "JAVASCRIPT", Year: "2022" },
-  { id: 4, Poster: "html.png", Title: "HTML5", Year: "2021" },
-  { id: 5, Poster: "css.png", Title: "CSS", Year: "2021" },
-  { id: 6, Poster: "php.png", Title: "PHP", Year: "2022" },
-  { id: 7, Poster: "wordpress.png", Title: "WORDPRESS", Year: "2022" },
-  { id: 8, Poster: "photoshop.png", Title: "PHOTOSHOP", Year: "2022" },
+  { id: 4, Poster: "tailwind.png", Title: "TAILWIND CSS", Year: "2021" },
+  { id: 5, Poster: "html.png", Title: "HTML5", Year: "2021" },
+  { id: 6, Poster: "css.png", Title: "CSS", Year: "2021" },
+  { id: 7, Poster: "php.png", Title: "PHP", Year: "2022" },
+  { id: 8, Poster: "wordpress.png", Title: "WORDPRESS", Year: "2022" },
+
 ];
 
 const projects = [
   {
     id: 1,
-    name: "www.animogifts.com",
+    name: "https://animogifts.com",
     description:
-      "An ecommerce website built with Wordpress, Vanilla JS and PHP",
-    image: "/8.png",
+      "Ecommerce Site",
+    image: "/animo1.png",
   },
   {
     id: 2,
     name: "https://to-do-app-ten-jade.vercel.app/",
-    description: "A to-do website built with React.",
-    image: "/check-list.png",
+    description: "To-do List",
+    image: "/animo2.png",
   },
   {
     id: 3,
     name: "https://usepopcorn-snowy.vercel.app/",
     description:
-      "A React IMBD clone where users can search and rate movies, Mobile responsive❌",
-    image: "clapperboard.png",
+      "IMBD clone",
+    image: "/animo3.png",
   },
   {
     id: 4,
     name: "https://sneakers-shop-mu.vercel.app/",
-    description: "A shoe ecommerce site, built with Vanilla JS.",
-    image: "shoes.png",
+    description: "shoepify",
+    image: "/animo4.png",
   },
   {
     id: 5,
     name: "https://consultancysite.vercel.app/",
-    description: "A consultancy site, built with HTML and CSS ",
-    image: "imagenew.png",
+    description: "consultancy site",
+    image: "/animo5.png",
   },
 ];
-const news = [
-  {
-    id: 1,
-    name: "Starting and Growing a Career in Web Design",
-    description:
-      "As the internet continues to develop and grow exponentially jobs related to the industry do too...",
-    image: "8.png",
-    year: "April 1, 2025",
-  },
-  {
-    id: 2,
-    name: "Create a Landing Page That Performs Great",
-    description:
-      "Whether you work in marketing, sales, or product design, you should understand the importance of a quality landing page...",
-    image: "Container3.png",
-    year: "October 8, 2024",
-  },
-  {
-    id: 3,
-    name: "How Can Designers Prepare for the Future?",
-    description:
-      "Last month I had the chance to attend CSS Day in Lagos, a two day event split between a “UI day” focusing on...",
-    image: "imagenew.png",
-    year: "Jan 4, 2023",
-  },
-  {
-    id: 4,
-    name: "Building a Navigation Component with Variables",
-    description:
-      " Navigation is key within any digital interface. Smart Components enable us to create custom interactive navigation...",
-    image: "last-image.png",
-    year: "Sep 5, 2022",
-  },
-];
+
 const investments = [
   {
     id: 1,
-    name: "Startup Unicorn",
+    name: "Bachelors Degree",
     description:
-      "This company specializes in making amazing things happen. Still in stealth and will launch in 2045.",
+      "University of Ibadan February, 2017 - December, 2021",
     image: "invest.png",
   },
   {
     id: 2,
-    name: "Exciting Company",
+    name: "Master of Arts",
     description:
-      "A company so incredible it will blow your socks off. They specialize in building things and moving mountains.",
+      "University of Ibadan September, 2024",
     image: "startup.png",
   },
 ];
@@ -125,38 +91,19 @@ const socials = [
   },
   {
     id: 2,
-    name: "LinkedIn",
-    description: "in/jakerobinson",
-    image: "social3.png",
-  },
-  {
-    id: 3,
-    name: "Instagram",
-    description: "ibk.adejumo",
-    image: "social4.png",
+    name: "Email",
+    description: "adejumoibk702@gmail.com",
+    image: "email.png",
   },
   {
     id: 4,
-    name: "Web",
-    description: "in/jakerobinson",
-    image: "social3.png",
+    name: "Phone",
+    description: "+234 913 351 6780",
+    image: "phone-call.png",
   },
 ];
 
-const services = [
-  {
-    id: 1,
-    Title: "Web Design",
-    Year: "Looking to build a website for your enterprise? I create efficient responsive websites that attract clients.",
-    Poster: "person-front-computer-working-html (1).png",
-  },
-  {
-    id: 2,
-    Title: "Product Design",
-    Year: "I dont just build websites, I am also an expert in product designing  with vast knowlege in using diffrent tools",
-    Poster: "photo-editor-histogram-setting-concept (1).png",
-  },
-];
+
 const review = [
   {
     id: 1,
@@ -253,12 +200,10 @@ function MainBody() {
       <Home />
       <WorkCard />
       <Projects />
-      <Services />
-      <Newsletter />
       <WhatPeopleSay />
       <Investments />
       <Socials />
-      <Footer />
+    
     </div>
   );
 }
@@ -346,39 +291,8 @@ function WorkList() {
     </ul>
   );
 }
-function Services() {
-  return (
-    <section className="services" id="services">
-      <div>
-        <h2>Services</h2>
-        <ServicesList />
-      </div>
-    </section>
-  );
-}
 
-function ServicesList() {
-  return (
-    <ul className="service-list">
-      {services.map((service) => (
-        <li key={service.id} className="service-item">
-          <div className="service-details">
-            <h3 className="service-title">{service.Title}</h3>
-            <p className="service-year">{service.Year}</p>
-          </div>
-          <img
-            src={service.Poster}
-            alt={`${service.Title} poster`}
-            className="service-poster"
-          />
-          <div>
-            <HoverButton />
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
-}
+
 
 type NavListProps = {
   closeSidebar: () => void;
@@ -414,8 +328,7 @@ function ProjectList() {
               className="project-image"
             />
             <div className="project-description">
-              <h3>{project.name}</h3>
-              <p>{project.description}</p>
+              <h3>{project.description}</h3>
             </div>
           </a>
         </li>
@@ -424,58 +337,9 @@ function ProjectList() {
   );
 }
 
-function HoverButton() {
-  return <button className="hover-button">Get started</button>;
-}
 
-function Newsletter() {
-  return (
-    <section className="newsletter" id="newsletter">
-      <div>
-        <h2>Newsletter</h2>
-        <div className="newsletter-sub">
-          <p>
-            I frequently relay my ideas to my subscriber list and keep them
-            informed about my endeavors—hoping to inspire someone. If you're
-            interested in such notifications, put your email below and get them
-            sent straight to your inbox.
-          </p>
-          <form
-            className="email-form"
-            action="your-server-endpoint"
-            method="POST"
-          >
-            <div className="input-container">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-                required
-              />
-              <button type="submit" className="submit-button">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-        <ul className="project-list">
-          {news.map((news) => (
-            <li key={news.id} className="news-item">
-              <img src={news.image} alt={news.name} className="news-image" />
-              <div className="news-description">
-                <h3>{news.name}</h3>
-                <p>{news.description}</p>
-                <p>{news.year}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <button className="hover-button2">View all articles</button>
-      </div>
-    </section>
-  );
-}
+
+
 
 function WhatPeopleSay() {
   return (
@@ -519,31 +383,18 @@ function WhatPeopleSay() {
           </li>
         ))}
       </ul>
-      <div className="peoplesay-box">
-        <div className="flexnamebox">
-          <div>
-            <img src="yuu.png" />
-          </div>
-          <div className="boxed-names">
-            <div className="name-rev">Afolabi Temilade</div>
-            <div className="pos-rev">Design Lead</div>
-          </div>
-        </div>
-        <p>
-          "Possessing exceptional design skills, a commendable work discipline,
-          superior problem-tackling tactics, and tremendous leadership traits,
-          Ibukun can add immense value to any contingent."
-        </p>
-      </div>
-      <button className="hover-button2">View all quotes</button>
+      
+      
+      
+     
     </section>
   );
 }
 
 function Investments() {
   return (
-    <section id="investments">
-      <h2>Investments</h2>
+    <section id="education">
+      <h2>Education</h2>
       <div>
         <ul className="project-list">
           {investments.map((investments) => (
@@ -563,8 +414,8 @@ function Investments() {
 
 function Socials() {
   return (
-    <section id="social">
-      <h2>Socials</h2>
+    <section id="contact">
+      <h2>Contact</h2>
       <div>
         <ul className="socials-list">
           {socials.map((social) => (
@@ -586,34 +437,5 @@ function Socials() {
   );
 }
 
-function Footer() {
-  return (
-    <section className="footer">
-      <div className="peoplesay-box">
-        <div className="flex-footer">
-          <div>
-            <h2>Partner with me</h2>
 
-            <p>
-              Are you seeking a designer to help you make leaps and boubds with
-              your business? Hire me to design experiences and build a brand
-              that users won't forget.
-            </p>
-            <div className="flex-btn-container2">
-              <ResusableButton className={`primary-btn1`}>
-                Lets's start a project
-              </ResusableButton>
-              <ResusableButton className={`Contact2`}>Contact</ResusableButton>
-            </div>
-          </div>
-          <div>
-            <div className="relative-position">
-              <img src="Overlay+Shadow.png" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
 export default App;
